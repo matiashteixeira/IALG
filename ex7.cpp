dac9f8005eaf02a8ea9324e779f26df956e6229d
matiashteixeira <mateus.teixeira1@estudante.ufla.br>
2022-11-01 10:17:36 -0300

    ex7

---
+++ ex7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float base, altura, area;
+    cin >> base >> altura;
+    area = base*altura/2;
+    cout << area;
+    return 0;
+}
+
