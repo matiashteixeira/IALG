daf89b65d972240280deef50998ad844172d38ab
matiashteixeira <mateus.teixeira1@estudante.ufla.br>
2022-11-01 10:53:31 -0300

    ex2

---
+++ ex2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+    float raio, area;
+    cin >> raio;
+    area = M_PI*pow(raio,2);
+    cout << area;
+    return 0;
+}
